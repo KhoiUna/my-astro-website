f2701f3f5cade5639c6581ae1476f4a7b9a951de
KhoiNguyen <thorwaitson@gmail.com>
2022-09-11 00:18:26 -0500

    init commit

---
+++ src/config.ts
@@ -0,0 +1,121 @@
+const config = {
+  brandName: "KHOI",
+  introduction:
+    "<b>I am</b> an international student from Vietnam pursuing a Bachelor's Degree in Computer Information Systems with a minor in Cybersecurity at University of North Alabama. <br/><b>I am</b> a Software Engineer that writes clean code and prioritizes good coding practices. Also, I believe my interest in Entrepreneurship help me stay open-minded to new ideas.",
+
+  // Please add .skill-icons
+  skills: [
+    {
+      name: "HTML",
+      icon: "logos:html-5",
+    },
+    {
+      name: "CSS",
+      icon: "logos:css-3",
+    },
+    {
+      name: "Bootstrap",
+      icon: "logos:bootstrap",
+    },
+    {
+      name: "TailwindCSS",
+      icon: "logos:tailwindcss-icon",
+    },
+    {
+      name: "Material UI",
+      icon: "logos:material-ui",
+    },
+    {
+      name: "JavaScript",
+      icon: "logos:javascript",
+    },
+    {
+      name: "TypeScript",
+      icon: "logos:typescript-icon",
+    },
+    {
+      name: "React",
+      icon: "logos:react",
+    },
+    {
+      name: "Redux",
+      icon: "logos:redux",
+    },
+    {
+      name: "NextJS",
+      icon: "logos:nextjs-icon",
+    },
+    {
+      name: "NodeJS",
+      icon: "logos:nodejs-icon",
+    },
+    {
+      name: "Apache",
+      icon: "logos:apache",
+    },
+    {
+      name: "Nginx",
+      icon: "logos:nginx",
+    },
+    {
+      name: "Linux",
+      icon: "flat-color-icons:linux",
+    },
+    {
+      name: "Python",
+      icon: "logos:python",
+    },
+    {
+      name: "Digital Ocean",
+      icon: "logos:digital-ocean",
+    },
+    {
+      name: "ExpressJS",
+      icon: "simple-icons:express",
+    },
+    {
+      name: "Google Cloud",
+      icon: "logos:google-cloud",
+    },
+    {
+      name: "MongoDB",
+      icon: "vscode-icons:file-type-mongo",
+    },
+    {
+      name: "MySQL",
+      icon: "logos:mysql",
+    },
+    {
+      name: "PostgreSQL",
+      icon: "logos:postgresql",
+    },
+  ],
+  contacts: [
+    {
+      type: "email",
+      text: "nguyentuankhoi2112@gmail.com",
+    },
+  ],
+
+  // Please add .social-icons
+  socials: [
+    {
+      link: "https://github.com/khoiUna/",
+      icon: `<i class="fa-brands fa-github social-icons"></i>`,
+    },
+    {
+      link: "https://twitter.com/KhoiUna",
+      icon: `<i class="fa-brands fa-twitter social-icons"></i>`,
+    },
+    {
+      link: "https://www.linkedin.com/in/khoiuna/",
+      icon: `<i class="fa-brands fa-linkedin-in social-icons"></i>`,
+    },
+    {
+      link: "https://www.youtube.com/c/khoiuna",
+      icon: `<i class="fa-brands fa-youtube social-icons"></i>`,
+    },
+  ],
+};
+
+export default config;
---
+++ src/scripts/script.ts
@@ -0,0 +1,70 @@
+import $ from "jquery";
+
+// Global variables
+const animationDuration = 300;
+let menuOpened = false;
+
+// Select elements
+const header = document.querySelector("header") as HTMLHeadingElement;
+
+// Functions
+const headerFading = () => {
+  if (menuOpened) return (header.style.animation = "colorFadeIn 1s forwards");
+
+  if (document.querySelector("html")!.scrollTop > 0)
+    return (header.style.animation = "colorFadeIn 1s forwards");
+
+  return (header.style.animation = "colorFadeOut 1s forwards");
+};
+
+const toggleMenu = () => {
+  if (!menuOpened) {
+    menuOpened = !menuOpened;
+    headerFading();
+
+    $("#burger-icon").fadeOut(animationDuration);
+    $("#dropdown-menu").fadeIn(animationDuration);
+    setTimeout(() => {
+      $("#close-icon").fadeIn(animationDuration);
+    }, animationDuration);
+  } else {
+    menuOpened = !menuOpened;
+    headerFading();
+
+    $("#close-icon").fadeOut(animationDuration);
+    $("#dropdown-menu").fadeOut(animationDuration);
+    setTimeout(() => {
+      $("#burger-icon").fadeIn(animationDuration);
+    }, animationDuration);
+  }
+};
+
+// Add jQuery
+window.addEventListener("scroll", headerFading);
+$(() => {
+  const smoothScroll = (cssSelector: string) => {
+    const destination = $(cssSelector);
+    const scrollPosition =
+      destination.offset()!.top - destination.offset()!.top * 0.05;
+
+    $("html").animate(
+      {
+        scrollTop: scrollPosition,
+      },
+      animationDuration
+    );
+  };
+
+  // nav-links
+  $(".nav-links").on("click", (event) => {
+    const destination = event.currentTarget.getAttribute("href")!;
+    smoothScroll(destination);
+    toggleMenu();
+  });
+
+  // dropdown-menu
+  $("#burger-menu").on("click", toggleMenu);
+
+  // read-more button
+  $("#read-more").on("click", () => smoothScroll("#introduction"));
+});
